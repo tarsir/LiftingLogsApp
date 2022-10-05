73274d7c49b0ad9001c588a08bac1c744b90c7c6
Stephen Hara <stephen.hara@stephenhara.com>
2022-10-05 19:27:27 +0900

    Add LICENSE and README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# LiftingLogs
+
+LiftingLogs is an open-source Android app for keeping track of your workouts and your lifting
+progress over time.
+
+## Contributing
+
+If you found this repo by looking for #hacktoberfest repositories, welcome! You may notice it's a
+very basic app. This app is partly a learning-in-public project for me, but may also be cool for
+someone else to help out with a maybe-neat app idea. Of course, if you're an experienced Android
+developer, your experience and help would be greatly appreciated!
+
+If you're interested in contributing, with a new feature, code review, bug fix, documentation, or 
+any other way, please make an issue first with a basic description of the feature! I'll take a look
+and we'll figure out how to get it started!
--- app/src/main/java/com/example/liftinglogs/MainActivity.kt
+++ app/src/main/java/com/example/liftinglogs/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.liftinglogs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import com.example.liftinglogs.ui.theme.LiftingLogsTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,14 +34,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Workout() {
+    var workout by remember { mutableStateOf("") }
+    var weight by remember { mutableStateOf(0)}
+    var reps by remember { mutableStateOf(0) }
+    var sets by remember { mutableStateOf(0) }
+
+    Row {
+        TextField(
+            modifier = Modifier.weight(3f),
+            value = workout,
+            onValueChange = { workout = it },
+            label = { Text("Workout") })
+        TextField(
+            modifier = Modifier.weight(1f),
+            value = weight.toString(),
+            onValueChange = { weight = it.toInt()  },
+            label = { Text("Weight")})
+        TextField(
+            modifier = Modifier.weight(1f),
+            value = reps.toString(),
+            onValueChange = { reps = it.toInt()  },
+            label = { Text("Reps")})
+        TextField(
+            modifier = Modifier.weight(1f),
+            value = sets.toString(),
+            onValueChange = { sets = it.toInt()  },
+            label = { Text("Sets")})
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Workout()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    LiftingLogsTheme {
-        Greeting("Android")
-    }
+    Greeting("Android")
 }
